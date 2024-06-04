8dca636424cc906877245ab0a26222ebcd41a8ea
farhoud.m7 <fymc5jspqhaqxwnt3z63myz6tv2kqls75xvtzgpbkh2e2unpl6vq>
2024-06-04 11:05:32 +0330

    test telegram bot features

--- main.py
+++ main.py
@@ -1,12 +1,55 @@
-import asyncio
-import telegram
+import logging
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, InlineQueryHandler
+from telegram import InlineQueryResultArticle, InputTextMessageContent
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-async def main():
-    bot = telegram.Bot("7409828464:AAFTgeEpiL7ffQKbdOi47U8HtZ3lNE7Zby8")
-    async with bot:
-        print(await bot.get_me())
+async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = []
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    await context.bot.answer_inline_query(update.inline_query.id, results)
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)    
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    application = ApplicationBuilder().token('7409828464:AAFTgeEpiL7ffQKbdOi47U8HtZ3lNE7Zby8').build()
+    
+    inline_caps_handler = InlineQueryHandler(inline_caps)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    caps_handler = CommandHandler('caps', caps)
+    start_handler = CommandHandler('start', start)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+    
+    application.add_handler(inline_caps_handler)
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    application.add_handler(caps_handler)
+    application.add_handler(unknown_handler)
+    
+    
+    application.run_polling()
