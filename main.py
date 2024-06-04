2d139a370c5272a0b532e466a0d30c0ab2015b5d
farhoud.m7 <fymc5jspqhaqxwnt3z63myz6tv2kqls75xvtzgpbkh2e2unpl6vq>
2024-06-04 11:27:37 +0330

    add topic id

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+
 async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.inline_query.query
     if not query:
@@ -22,15 +23,21 @@ async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
     await context.bot.answer_inline_query(update.inline_query.id, results)
 
+
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
+
 async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text_caps = ' '.join(context.args).upper()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)    
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    thread_id = update.message.message_thread_id
+    await context.bot.send_message(chat_id=update.effective_chat.id, message_thread_id=thread_id,
+                                   text="I'm a bot, please talk to me!")
+
 
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
@@ -38,18 +45,17 @@ async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token('7409828464:AAFTgeEpiL7ffQKbdOi47U8HtZ3lNE7Zby8').build()
-    
+
     inline_caps_handler = InlineQueryHandler(inline_caps)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     caps_handler = CommandHandler('caps', caps)
     start_handler = CommandHandler('start', start)
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
-    
+
     application.add_handler(inline_caps_handler)
     application.add_handler(start_handler)
     application.add_handler(echo_handler)
     application.add_handler(caps_handler)
     application.add_handler(unknown_handler)
-    
-    
+
     application.run_polling()
